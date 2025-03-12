71d5ead3d78ac52b456426751af55466c1f66392
MaxLastBreath <yusei.tqnev@gmail.com>
2025-03-12 11:25:02 +0200

    Footer Update

--- js/autofooter.js
+++ js/autofooter.js
@@ -1,12 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
     const footer = document.createElement('footer');
     
-    footer.innerHTML = `
-        <hr>
-        <p>NX-Optimizer, Ultracam © 2025 MaxLastBreath. All rights reserved.</p>
-        <p>This is an independent, third-party project developed through reverse engineering. </p>
-        <p>It does not include, distribute, or modify any original assets, code, or intellectual property belonging to any third party.</p>
-        <p>All trademarks, logos, and intellectual property referenced remain the property of their respective owners. This project is not affiliated with, endorsed by, or approved by any rights holders.</p>
+    footer.innerHTML = 
+    `
+        <div class="footer-container">
+            <div class="copyright">
+                Copyright © 2025 MaxLastBreath
+            </div>
+        </div>
     `;
 
     document.body.appendChild(footer);
---
+++ js/mainfooter.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const footer = document.createElement('footer');
+    
+    footer.innerHTML = 
+    `
+        <div class="footer-container">
+        <div class="left-content">
+            <p><a href="/" class="logo"><img src="/Pictures/logo_main.png" style="height: 35px;" id="logo"></a></p>
+            <p style="margin-bottom: 10px">
+                NX Optimizer provides game optimization mods and freecamera for: Tears Of The Kingdom, 
+                Breath Of The Wild, Echoes of Wisdom, Link's Awakening, 
+                Pokemon Scarlet And Violet and other games. 
+                Everything you need in one place.
+            </p>
+        </div>
+        <div class="right-content">
+            <div>
+                <p><b>Pages</b></p>
+                <p><a class="custom-nav underline-hover" href="/">Home</a></p>
+                <p><a class="custom-nav underline-hover" href="/guides/setup-guide/">Guides</a></p>
+            </div>
+            <div>
+                <p><b>Social Media</b></p>
+                <p><a class="custom-nav underline-hover" href="discord">Discord</a></p>
+                <p><a class="custom-nav underline-hover" href="ko-fi">Ko-fi</a></p>
+                <p><a class="custom-nav underline-hover" href="patreon">Patreon</a></p>
+            </div>
+        </div>
+        <div class="copyright">
+            NX-Optimizer, Ultracam © 2025 MaxLastBreath
+        </div>
+    `;
+
+    footer.classList.add("main-footer");
+
+    document.body.appendChild(footer);
+});
